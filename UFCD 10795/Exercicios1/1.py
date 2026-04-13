92e547aaef12b93943a7fa806d66dc93285ce226
Guilherme Cabrita <gui.celorindo@gmail.com>
2026-04-13 11:13:40 +0100

    Ex1 - 1 correção

--- UFCD 10795/Exercicios1/1.py
+++ UFCD 10795/Exercicios1/1.py
@@ -1,19 +1,9 @@
 dia = input("Insira um dia da semana ('xxxx-Feira' ou 'Sábado' etc...)\nR: ")
 
 match dia:
-    case "Segunda-Feira":
+    case "Segunda-Feira" | "Terça-Feira" | "Quarta-Feira" | "Quinta-Feira" | "Sexta-Feira":
         print("Dia útil")
-    case "Terça-Feira":
-        print("Dia útil")
-    case "Quarta-Feira":
-        print("Dia útil")
-    case "Quinta-Feira":
-        print("Dia útil")
-    case "Sexta-Feira":
-        print("Dia útil")
-    case "Sábado":
-        print("Fim de semana")
-    case "Domingo":
+    case "Sábado" | "Domingo":
         print("Fim de semana")
     case _:
         print("Esse dia não existe ou está mal escrito.")
